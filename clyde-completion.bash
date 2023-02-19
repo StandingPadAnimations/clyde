42d76d8217c386161c7c57a2c56984ebfa2eb0e9
mahid <mahidsheikh@pm.me>
2023-02-19 16:01:16 -0600

    Finished autocompletion

--- clyde-completion.bash
+++ clyde-completion.bash
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+LOCAL_PKGS=$(pacman -Qm | cut -f 1 -d " ") # Do it once to get it out of the way
 _clyde_completions()
 {
-  if [ "${#COMP_WORDS[@]}" != "2" ]; then
+  ARG="${COMP_WORDS[1]}"
+  if [[ "$ARG" = "remove" || "$ARG" = "rm" || "$ARG" = "yeet" || "$ARG" = "upgrade" || "$ARG" = "upg" ]]; then
+    compgen -W "$LOCAL_PKGS" "${COMP_WORDS[2]}"
     return
   fi
   compgen -W "install remove upgrade help" "${COMP_WORDS[1]}"
